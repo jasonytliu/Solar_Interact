e25f53865f17a4940b5ac1f66dab7e285bfddd34
jonbradshawcph <jonbradshawcph@gmail.com>
2017-03-11 16:18:13 -0800

    Update Solar_Interact.ino
    Added a powerpolling function

--- Solar_Interact.ino
+++ Solar_Interact.ino
@@ -12,6 +12,16 @@
 
 /******************Functions*************************/
 
+float pollPower(int pinIn){
+  int Vint;
+  float Power;
+  float Vflo;
+  Vint = analogRead(pinIn);
+  Vflo = 5.0*(Vint/1024.0);
+  Power = 1000*(16.0*(Vflo*Vflo))/20000.0;
+  return Power;
+}
+
 /******************Helpers**************************/
 
 /** Performs any pinMode() definitions normally done in setup()
