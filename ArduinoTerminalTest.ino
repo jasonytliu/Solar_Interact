3805d88d72dc0ce0ce8537547ec297093c7ebf7a
msnair97 <msnair@ucsd.edu>
2017-05-21 16:01:13 -0700

    Add files via upload
    Partially working terminal

---
+++ ArduinoTerminalTest.ino
@@ -0,0 +1,27 @@
+String readString;
+int c;
+void setup()
+{
+ pinMode(0, INPUT_PULLUP);// only needed for JY-MCUY v1.06?
+ Serial.begin(9600);
+ Serial.println("OK then, you first, say something.....");
+ Serial.println("Go on, type something and hit Send,");
+ Serial.println("or just hit the Enter key,");
+ Serial.println("then I will repeat it!");
+ Serial.println("");
+}
+void loop() {
+ while (Serial.available())
+ {
+ delay(3);
+ Serial.print(Serial.parseInt());
+// c = Serial.read();
+ readString += c;
+ }// end while
+ if (readString.length() >0)
+ {
+ Serial.write(c);
+ readString="";
+ } // end if
+} 
+
